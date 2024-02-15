1c8ae80937ee42606accec3267d5ab3b12a711d8
PaulAroo <timmypaul2006@gmail.com>
2024-02-15 18:24:35 +0100

    feat: loading states for host and clients

--- s/views/Client/ClientView.ts
+++ s/views/Client/ClientView.ts
@@ -3,6 +3,7 @@ import { html } from "@benev/slate"
 import { baseURL } from "../../config.js"
 import { app } from "../../context/app.js"
 import { SessionInfo } from "../../types.js"
+import { Spinner } from "../spinner/spinner.js"
 import { joinCallSession } from "./utils/joinCallSession.js"
 
 interface ClientViewProps {
@@ -16,12 +17,14 @@ export const ClientView = app.light_view((use) => (props: ClientViewProps) => {
 	const { sessionId, audioElement, signalServerUrl } = props
 
 	const [clientId, setClientId] = use.state("")
+	const [loading, setLoading] = use.state(false)
 	const [errorOccured, setErrorOccured] = use.state(false)
 	const [sessionDetails, setSessionDetails] = use.state<
 		SessionInfo | undefined
 	>(undefined)
 
 	use.mount(() => {
+		setLoading(true)
 		;(async () => {
 			try {
 				const { clientId, sessionInfo } = await joinCallSession({
@@ -30,11 +33,12 @@ export const ClientView = app.light_view((use) => (props: ClientViewProps) => {
 					signalServerUrl,
 					handleDisconnect,
 				})
-
 				setClientId(clientId)
 				setSessionDetails(sessionInfo)
+				setLoading(false)
 			} catch (error) {
 				setErrorOccured(true)
+				setLoading(false)
 				handleDisconnect()
 			}
 		})()
@@ -49,6 +53,10 @@ export const ClientView = app.light_view((use) => (props: ClientViewProps) => {
 			track.stop()
 		})
 		setSessionDetails(undefined)
+	}
+
+	if (loading) {
+		return html` ${Spinner([])}`
 	}
 
 	return html`
--- s/views/Host/HostView.ts
+++ s/views/Host/HostView.ts
@@ -1,6 +1,7 @@
 import { html } from "@benev/slate"
 
 import { app } from "../../context/app.js"
+import { Spinner } from "../spinner/spinner.js"
 import { PeerConnection, SessionInfo } from "../../types.js"
 import { createCallSession } from "./utils/createCallSession.js"
 import { renderSessionDetails } from "./utils/renderSessionDetails.js"
@@ -14,6 +15,7 @@ export const HostView = app.light_view((use) => (props: HostViewProps) => {
 	use.name("host-view")
 
 	const { audioElement, signalServerUrl } = props
+	const [loading, setLoading] = use.state(false)
 	const [sessionDetails, setSessionDetails] = use.state<
 		SessionInfo | undefined
 	>(undefined)
@@ -21,14 +23,17 @@ export const HostView = app.light_view((use) => (props: HostViewProps) => {
 
 	const startCallSession = async () => {
 		try {
+			setLoading(true)
 			const session = await createCallSession({
 				audioElement,
 				signalServerUrl,
 				peerConnections,
 			})
 			setSessionDetails(session)
+			setLoading(false)
 		} catch (error) {
 			console.log(error)
+			setLoading(false)
 		}
 	}
 
@@ -46,10 +51,16 @@ export const HostView = app.light_view((use) => (props: HostViewProps) => {
 
 	return html`
 		<div>Host a call session</div>
-		<button @click=${startCallSession} .disabled=${!!sessionDetails}>
-			start
-		</button>
-		<button @click=${stopCallSession} .disabled=${!sessionDetails}>stop</button>
-		${renderSessionDetails(sessionDetails, peerConnections.value)}
+		${loading
+			? html`${Spinner([])}`
+			: html`
+					<button @click=${startCallSession} .disabled=${!!sessionDetails}>
+						start
+					</button>
+					<button @click=${stopCallSession} .disabled=${!sessionDetails}>
+						stop
+					</button>
+					${renderSessionDetails(sessionDetails, peerConnections.value)}
+			  `}
 	`
 })
