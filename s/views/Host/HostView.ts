a4d2f7a6961f8111807a8b9dc2c6ec56557c45f4
PaulAroo <timmypaul2006@gmail.com>
2024-02-24 19:43:59 +0100

    refactor: clean up host view

--- s/views/Host/HostView.ts
+++ s/views/Host/HostView.ts
@@ -2,61 +2,29 @@ import { html } from "@benev/slate"
 
 import { app } from "../../context/app.js"
 import { Spinner } from "../spinner/Spinner.js"
-import { Peer, SessionInfo } from "../../types.js"
-// import { createCallSession } from "./utils/createCallSession.js"
 import { renderSessionDetails } from "./utils/renderSessionDetails.js"
 
 interface HostViewProps {
 	audioElement: HTMLAudioElement
 }
 
-// rerender when
-/*
-- session changes
-- client's peer conn state change
-- 
-*/
-
 export const HostView = app.light_view((use) => (props: HostViewProps) => {
 	use.name("host-view")
 	const { audioElement } = props
-
-	const session = use.watch(() => use.context.state.session)
-
 	const [loading, setLoading] = use.state(false)
-	// const [sessionDetails, setSessionDetails] = use.state<
-	// SessionInfo | undefined
-	// >(undefined)
-	// const peerConnections = use.signal(new Map<string, Peer>())
+	const session = use.watch(() => use.context.state.session)
 
 	const clients = use.context.clients
 	const { startCall, endCall } = use.context.actions.host
 	const startCallSession = async () => {
 		try {
 			setLoading(true)
-			// const session = await createCallSession({
-			// 	audioElement,
-			// 	peerConnections,
-			// })
 			await startCall(audioElement)
-			// setSessionDetails(session)
 			setLoading(false)
 		} catch (error) {
 			console.log(error)
 			setLoading(false)
 		}
-	}
-
-	const stopCallSession = () => {
-		// const { localStream, terminateSession } = use.context.host
-		// peerConnections.value.forEach(({ peer }) => {
-		// 	peer.close()
-		// })
-		// localStream?.getTracks().forEach((track) => {
-		// 	track.stop()
-		// })
-		// terminateSession()
-		// setSessionDetails(undefined)
 	}
 
 	return html`
